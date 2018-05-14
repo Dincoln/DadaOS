acc0de28c9724b6665a604e6e7d915f252072a4b
Dincoln <dincolm@163.com>
2018-05-14 11:11:05 +0800

    Initial Commit

---
+++ DataOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DataOS
+//
+//  Created by Dincoln on 2018/5/14.
+//  Copyright © 2018年 Dincoln. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ DataOS/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  DataOS
+//
+//  Created by Dincoln on 2018/5/14.
+//  Copyright © 2018年 Dincoln. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
